930141b8e2bca45897cded0ad7397b304ecebfe1
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-01-24 18:09:35 +0530

    Pie Charts and meenu

--- matplotlib-sentdex/lec1.py
+++ matplotlib-sentdex/lec1.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
-from random import randint
-x = [randint(0, 9) for i in range(10)]
-y = [randint(0, 9) for i in range(10)]
 
-plt.scatter(x,y,label='choo',color='k')
-plt.xlabel('x')
-plt.ylabel('y')
+slices = [2,45,52,5,4]
+
+people = ['akash','kanika','malti','mahesh','meenu']
+cols = ['c','m','r','b','k']
+
+plt.pie(slices,
+        labels=people,
+        colors=cols,
+        startangle=90,
+        shadow=True,
+        explode=(0,0,0,0,0.1),
+        autopct='%.2f'
+)
+
+#plt.xlabel('x')
+#plt.ylabel('y')
 plt.title('Interesting Graph\nCheck it out')
-plt.legend()
+#plt.legend()
 plt.show()
