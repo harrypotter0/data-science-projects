9ea20c28f531392d6985c2e79894c4d434ec9bd9
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-01-24 17:19:56 +0530

    Histograms

--- matplotlib-sentdex/lec1.py
+++ matplotlib-sentdex/lec1.py
@@ -1,15 +1,10 @@
 import matplotlib.pyplot as plt
 
-x = [2,4,6,8,10]
-y = [2,4,5,5,5]
+population_ages = [22,55,62,45,21,22,34,42,42,4,99,102,110,120,121,122,130,111,115,112,80,75,65,54,44,43,42,48]
 
-y2 = [2,4,6,8,10]
-x2 = [2,4,5,5,5]
-
-
-plt.bar(x,y,label='Bars1')
-plt.bar(x2,y2,label='Bars2')
-
+#bins = [0,10,20,30,40,50,60,70,80,90,100,110,120,130]
+bins = [10*x for x in range(14)]
+plt.hist(population_ages, bins, histtype='bar', rwidth=0.8)
 
 plt.xlabel('x')
 plt.ylabel('y')
