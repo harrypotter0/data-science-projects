229a2856cbd2f589929f876f406e8ac3c4b99cb8
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-01-24 17:15:04 +0530

    Bar chart

--- matplotlib-sentdex/lec1.py
+++ matplotlib-sentdex/lec1.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 
-x = [1,2,3]
-y = [5,7,4]
+x = [2,4,6,8,10]
+y = [2,4,5,5,5]
 
-x2 = [1,2,3]
-y2 = [10,14,12]
+y2 = [2,4,6,8,10]
+x2 = [2,4,5,5,5]
 
-plt.plot(x, y, label='First Line')
-plt.plot(x2, y2, label='Second Line')
 
-plt.xlabel('Plot Number')
-plt.ylabel('Important var')
+plt.bar(x,y,label='Bars1')
+plt.bar(x2,y2,label='Bars2')
+
+
+plt.xlabel('x')
+plt.ylabel('y')
 plt.title('Interesting Graph\nCheck it out')
 plt.legend()
 plt.show()
