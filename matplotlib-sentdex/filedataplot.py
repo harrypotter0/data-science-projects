8dffa895dafc7cec4efa58caf36cf5fdb191e1a2
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-01-24 18:39:34 +0530

    Csv loaded into plots

--- matplotlib-sentdex/filedataplot.py
+++ matplotlib-sentdex/filedataplot.py
@@ -8,15 +8,20 @@ import csv
 x = []
 y = []
 
-with open('example.txt', 'r') as csvfile:
+with open('example.txt','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x.append(int(row[0]))
         y.append(int(row[1]))
+
+plt.plot(x,y,label='Loaded from file!')
 '''
 
-x, y = np.loadtxt('example.txt', delimiter=',', unpack=True)
-plt.plot(x,y, label='Loaded from file')
+x,y = np.loadtxt('example.txt', delimiter=',',unpack=True)
+plt.plot(x,y,label='Loaded from file!')
+
+
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Interesting Graph\n')
